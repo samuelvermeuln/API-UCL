2e866ecdb4d9690554c129f438603092f91f675b
samuel vermeuln <70477826+samuelvermeuln@users.noreply.github.com>
2022-01-05 23:12:07 -0300

    🚀  🚀  🚀

--- api/index.js
+++ api/index.js
@@ -4,136 +4,139 @@ const puppeteer = require("puppeteer");
 
 async function BuscaDados(login, senha) {
 
-  const browser = await puppeteer.launch();
-  
-  const page = await browser.newPage();
+  try {
+    const browser = await puppeteer.launch();
 
-  await page.goto("https://eies.ucl.br/webaluno/login/");
+    const page = await browser.newPage();
 
-  await page.type("#user", `${login}`);
-  await page.type("#password", `${senha}`);
+    await page.goto("https://eies.ucl.br/webaluno/login/");
 
-  await page.click('[type="submit"]');
+    await page.type("#user", `${login}`);
+    await page.type("#password", `${senha}`);
 
-  await page.waitForNavigation();
+    await page.click('[type="submit"]');
 
-  await page.goto("https://eies.ucl.br/webaluno/quadrodenotas/");
+    await page.waitForNavigation();
 
-  await page.waitForTimeout(2000);
+    await page.goto("https://eies.ucl.br/webaluno/quadrodenotas/");
 
-  await page.click('[class="collapsible-header active"]');
+    await page.waitForTimeout(2000);
 
-  await page.waitForTimeout(1500);
+    await page.click('[class="collapsible-header active"]');
 
-  const notas_user = await page.evaluate(() => {
-    DadosUser = [];
-    DadosNotas = [];
-    json = [];
+    await page.waitForTimeout(1500);
 
-    try {
-      const quantidade_de_materias = document
-        .querySelector("#\\32 021-2 > ul")
-        .innerText.split(/[\n,]+/)
-        .filter((v) => v !== "keyboard_arrow_right")
-        .filter((v) => v !== "").length;
+    const notas_user = await page.evaluate(() => {
+      DadosUser = [];
+      DadosNotas = [];
+      json = [];
 
-      // const Numerocoluna = document.querySelector(
-      // "#\\32 021-2 > ul > li.active > div.collapsible-body > table > tbody"
-      // ).rows.length;
+      try {
+        const quantidade_de_materias = document
+          .querySelector("#\\32 021-2 > ul")
+          .innerText.split(/[\n,]+/)
+          .filter((v) => v !== "keyboard_arrow_right")
+          .filter((v) => v !== "").length;
 
-      for (let i = 1; i < quantidade_de_materias + 1; i++) {
-        const nome_Materia = document
-          .querySelector(
-            `#\\32 021-2 > ul > li:nth-child(${i}) > div.collapsible-header`
-          )
-          .innerText.split(/\s/g)
-          .filter((v) => v !== "keyboard_arrow_right");
+        // const Numerocoluna = document.querySelector(
+        // "#\\32 021-2 > ul > li.active > div.collapsible-body > table > tbody"
+        // ).rows.length;
 
-        const nome_Professor = document
-          .querySelector(
-            `#\\32 021-2 > ul > li:nth-child(${i}) > div.collapsible-body > ul > li.collection-item.dismissable > div`
-          )
-          .innerText.replace(/\s/g, "");
-
-        const numeroNotas = document.querySelector(
-          `#\\32 021-2 > ul > li:nth-child(${i}) > div.collapsible-body > table > tbody`
-        ).rows.length;
-
-        DadosUser.push({
-          dados: {
-            nome_Materia: nome_Materia,
-            nome_Professor: nome_Professor,
-          },
-        });
-
-        for (let j = 1; j < numeroNotas + 1; j++) {
-          const grupo = document
+        for (let i = 1; i < quantidade_de_materias + 1; i++) {
+          const nome_Materia = document
             .querySelector(
-              `#\\32 021-2 > ul > li:nth-child(${i}) > div.collapsible-body > table > tbody > tr:nth-child(${j}) > td:nth-child(1)`
+              `#\\32 021-2 > ul > li:nth-child(${i}) > div.collapsible-header`
+            )
+            .innerText.split(/\s/g)
+            .filter((v) => v !== "keyboard_arrow_right");
+
+          const nome_Professor = document
+            .querySelector(
+              `#\\32 021-2 > ul > li:nth-child(${i}) > div.collapsible-body > ul > li.collection-item.dismissable > div`
             )
             .innerText.replace(/\s/g, "");
 
-          const data = document
-            .querySelector(
-              `#\\32 021-2 > ul > li:nth-child(${i}) > div.collapsible-body > table > tbody > tr:nth-child(${j}) > td:nth-child(2)`
-            )
-            .innerText.replace(/\s/g, "");
+          const numeroNotas = document.querySelector(
+            `#\\32 021-2 > ul > li:nth-child(${i}) > div.collapsible-body > table > tbody`
+          ).rows.length;
 
-          const avalicao = document
-            .querySelector(
-              `#\\32 021-2 > ul > li:nth-child(${i}) > div.collapsible-body > table > tbody > tr:nth-child(${j}) > td:nth-child(3)`
-            )
-            .innerText.replace(/\s/g, "");
-
-          const peso = document
-            .querySelector(
-              `#\\32 021-2 > ul > li:nth-child(${i}) > div.collapsible-body > table > tbody > tr:nth-child(${j}) > td:nth-child(4)`
-            )
-            .innerText.replace(/\s/g, "");
-
-          const nota = document
-            .querySelector(
-              `#\\32 021-2 > ul > li:nth-child(${i}) > div.collapsible-body > table > tbody > tr:nth-child(${j}) > td:nth-child(5)`
-            )
-            .innerText.replace(/\s/g, "");
-
-          const faltouSemestral = document
-            .querySelector(
-              `#\\32 021-2 > ul > li:nth-child(${i}) > div.collapsible-body > table > tbody > tr:nth-child(${j}) > td:nth-child(7)`
-            )
-            .innerText.replace(/\s/g, "");
-
-          DadosNotas.push({
-            notas: {
-              grupo: grupo,
-              data: data,
-              avalicao: avalicao,
-              peso: peso,
-              nota: nota,
-              faltouSemestral: faltouSemestral,
+          DadosUser.push({
+            dados: {
+              nome_Materia: nome_Materia,
+              nome_Professor: nome_Professor,
             },
           });
+
+          for (let j = 1; j < numeroNotas + 1; j++) {
+            const grupo = document
+              .querySelector(
+                `#\\32 021-2 > ul > li:nth-child(${i}) > div.collapsible-body > table > tbody > tr:nth-child(${j}) > td:nth-child(1)`
+              )
+              .innerText.replace(/\s/g, "");
+
+            const data = document
+              .querySelector(
+                `#\\32 021-2 > ul > li:nth-child(${i}) > div.collapsible-body > table > tbody > tr:nth-child(${j}) > td:nth-child(2)`
+              )
+              .innerText.replace(/\s/g, "");
+
+            const avalicao = document
+              .querySelector(
+                `#\\32 021-2 > ul > li:nth-child(${i}) > div.collapsible-body > table > tbody > tr:nth-child(${j}) > td:nth-child(3)`
+              )
+              .innerText.replace(/\s/g, "");
+
+            const peso = document
+              .querySelector(
+                `#\\32 021-2 > ul > li:nth-child(${i}) > div.collapsible-body > table > tbody > tr:nth-child(${j}) > td:nth-child(4)`
+              )
+              .innerText.replace(/\s/g, "");
+
+            const nota = document
+              .querySelector(
+                `#\\32 021-2 > ul > li:nth-child(${i}) > div.collapsible-body > table > tbody > tr:nth-child(${j}) > td:nth-child(5)`
+              )
+              .innerText.replace(/\s/g, "");
+
+            const faltouSemestral = document
+              .querySelector(
+                `#\\32 021-2 > ul > li:nth-child(${i}) > div.collapsible-body > table > tbody > tr:nth-child(${j}) > td:nth-child(7)`
+              )
+              .innerText.replace(/\s/g, "");
+
+            DadosNotas.push({
+              notas: {
+                grupo: grupo,
+                data: data,
+                avalicao: avalicao,
+                peso: peso,
+                nota: nota,
+                faltouSemestral: faltouSemestral,
+              },
+            });
+          }
+
+          const array1 = DadosUser.concat(DadosNotas);
+
+          json.push({ obj: array1 });
+
+          DadosUser.splice(0, DadosUser.length);
+          DadosNotas.splice(0, DadosNotas.length);
         }
 
-        const array1 = DadosUser.concat(DadosNotas);
-
-        json.push({ obj: array1 });
-
-        DadosUser.splice(0, DadosUser.length);
-        DadosNotas.splice(0, DadosNotas.length);
+        return json;
+      } catch (error) {
+        return error.message;
       }
+    });
 
-      return json;
-    } catch (error) {
-      return error.message;
-    }
-  });
+    await browser.close();
 
-  await browser.close();
+    return JSON.stringify(notas_user);
 
-  return JSON.stringify(notas_user);
+  } catch (error) {
+    return error.message;
+  }
+  
 }
-
-
-
 
